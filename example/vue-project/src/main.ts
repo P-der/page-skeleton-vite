fcdd2599be9b7cfb0f89cff355cd6413a1b0ae6b
pder <1198811801@qq.com>
2022-06-21 21:09:05 +0800

    add code

--- example/vue-project/src/main.ts
+++ example/vue-project/src/main.ts
@@ -1,4 +1,5 @@
+import 'amfe-flexible/index.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app1')
--- example/vue-project/vite.config.ts
+++ example/vue-project/vite.config.ts
@@ -5,7 +5,20 @@ import vue from '@vitejs/plugin-vue'
 import testPlug from '../../src/index'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),testPlug()],
+  css: {
+        preprocessorOptions: {
+          less: {
+          javascriptEnabled: true
+        }
+     }
+  },
+  plugins: [
+    vue(),
+    testPlug({
+      port: '8081',
+      routes: ['/']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
